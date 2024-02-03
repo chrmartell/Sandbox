b5f1387dc8f3b9e723e35d9d393fc09a253d1069
chrmartell <45318705+chrmartell@users.noreply.github.com>
2024-02-03 14:26:42 -0800

    03Feb2024_commit
    Created a custom task to grab boss phase

---
+++ Assets/Scripts/CustomTasks/GetBossPhase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class GetBossPhase : Conditional
+{
+    public int conditionalBossPhase;
+
+    private GameManager gameManager;
+
+    public override void OnStart()
+    {
+        // Get a reference to the GameManager object
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (gameManager != null)
+        {
+            int currentBossPhase = gameManager.CurrentBossPhase;
+
+            // Check if the currentBossPhase is equal to conditionalBossPhase
+            if (currentBossPhase == conditionalBossPhase)
+            {
+                return TaskStatus.Success;
+            }
+        }
+
+        // If GameManager is not found or currentBossPhase doesn't match conditionalBossPhase, return failure
+        return TaskStatus.Failure;
+    }
+}
